3e6b31b3de3a907de8bf28f51e6906241da1e056
changhwan77 <ckdghks9741@gmail.com>
2023-04-04 01:06:58 +0900

    간단한 메인화면 위젯 구성

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(WhatTodayRiceApp());
+}
+
+class WhatTodayRiceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('세종기숙사'),
+        ),
+      ),
+    );
+  }
+}
