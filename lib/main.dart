ae5f756ba7886c1b8a82db106fc01a58361e2ac0
changhwan77 <ckdghks9741@gmail.com>
2023-04-05 10:50:38 +0900

    WHAT-15 알림 설정 화면 완료 UI 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import './view/screens/completed.dart';
 
 void main() {
   runApp(WhatTodayRiceApp());
 }
 
 class WhatTodayRiceApp extends StatelessWidget {
+  const WhatTodayRiceApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('세종기숙사'),
+          title: Text('세종 기숙사'),
+          centerTitle: true,
+        ),
+        body: Container(
+          child: Text('오밥뭐'),
         ),
       ),
     );
