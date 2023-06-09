f406fe01816628978dc5f58151ea147ce93b9ecc
hyeonhh <tjdgustjdan@gmail.com>
2023-06-09 22:32:43 +0900

    🗃️ 식단 db 연결 확인
    식단 db 연결 후 데이터가 사용가능한지 확인완료했습니다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,52 @@
-import 'package:flutter/material.dart';
 //import 'package:flutter_whattodayrice/view/screens/selectdormitory.dart';
 //import 'package:flutter_whattodayrice/screens/selectdormitory.dart';
 //import 'package:flutter_whattodayrice/view/screens/table_calendar_practice.dart';
-import 'package:flutter_whattodayrice/view/screens/home_screen.dart';
-import 'package:flutter_whattodayrice/view/screens/settings_screen.dart';
 import 'dart:io';
-import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+Future<void> main() async {
+  databaseFactory = databaseFactoryFfi;
+  String dbPath = 'assets/db/meal.db';
+  bool dbExists = File(dbPath).existsSync();
+  if (!dbExists) {
+    print('db파일이 존재하지 않습니다.');
+    return;
+  }
+  Database database = await openDatabase(dbPath, readOnly: true);
+
+  List<Map<String, dynamic>> results = await database.query('menu');
+  List<Map<String, dynamic>> sejongResults =
+      await database.query('sejong_menu');
+
+  print('행복db: $results');
+  print('세종db: $sejongResults');
+
+  await database.close();
+
   HttpClient httpClinet = HttpClient();
   httpClinet.badCertificateCallback =
       (X509Certificate cert, String host, int port) => true;
-
-  initializeDateFormatting().then((_) {
-    runApp(const MyApp());
-  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        SettingsScreen.routeName: (context) => const SettingsScreen(),
-      },
-      // theme: kTextStyleGuide,
-      debugShowCheckedModeBanner: false,
-      home: const Scaffold(body: HomeScreen()),
-    );
-  }
-}
+
+  // initializeDateFormatting().then((_) {
+  //   runApp(const MyApp());
+  // });
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       routes: {
+//         // SettingsScreen.routeName: (context) => const SettingsScreen(),
+//       },
+//       // theme: kTextStyleGuide,
+//       debugShowCheckedModeBanner: false,
+//       // home: Scaffold(body: HomeScreen()),
+//     );
+//   }
+// }
