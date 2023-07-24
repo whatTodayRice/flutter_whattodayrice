e5cd94b36948f5619b83a1859c1a3f03583752ff
changhwan77 <ckdghks9741@gmail.com>
2023-07-24 22:31:14 +0900

    feat: 오늘의 식단 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,22 @@
 //import 'package:flutter_whattodayrice/screens/selectdormitory.dart';
 //import 'package:flutter_whattodayrice/view/screens/table_calendar_practice.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_whattodayrice/services/sejong_fetch_meal_db.dart';
+import 'package:flutter_whattodayrice/models/dummy_happy_meal.dart';
+import 'package:flutter_whattodayrice/services/happy_fetch_meal_from_db.dart';
 import 'package:flutter_whattodayrice/view/screens/home_screen.dart';
+
+import 'models/happy_meal.dart';
 
 void main() async {
   // HttpClient httpClinet = HttpClient();
   // httpClinet.badCertificateCallback =
   //     (X509Certificate cert, String host, int port) => true;
-  DateTime now = DateTime.now();
-  List<MealData?> weeklyMeals = await fetchWeeklyMealsFromDb(now);
-
-  print(weeklyMeals);
-  runApp(MyApp(weeklyMeals));
+  dummyWeeklyData;
+  runApp(MyApp(dummyWeeklyData));
 }
 
 class MyApp extends StatelessWidget {
-  final List<MealData?> weeklyMeals;
+  final List<HappyMealData?> weeklyMeals;
 
   const MyApp(this.weeklyMeals, {super.key});
 
