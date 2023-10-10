88ebca22bfc745c87061273e7c6a0aef08c66fd8
hyeonhh <tjdgustjdan@gmail.com>
2023-10-10 23:27:43 +0900

    ✨ 바로가기 위젯을 만들었어요!
    위젯을 클릭하면 앱이 실행되도록 했습니다 !!
    
    -JIRA : WHAT-107

--- android/app/src/main/java/com/example/flutter_whattodayrice/MealWidget.kt
+++ android/app/src/main/java/com/example/flutter_whattodayrice/MealWidget.kt
@@ -73,15 +73,15 @@ class MealWidget : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.meal_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
-}
+//internal fun updateAppWidget(
+//    context: Context,
+//    appWidgetManager: AppWidgetManager,
+//    appWidgetId: Int
+//) {
+//    val widgetText = context.getString(R.string.appwidget_text)
+//    // Construct the RemoteViews object
+//    val views = RemoteViews(context.packageName, R.layout.meal_widget)
+//    views.setTextViewText(R.id.appwidget_text, widgetText)
+//    // Instruct the widget manager to update the widget
+//    appWidgetManager.updateAppWidget(appWidgetId, views)
+//}
