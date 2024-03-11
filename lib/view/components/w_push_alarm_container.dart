50e378fb2868e28ab53c321641e7f09f73e246c1
changhwan77 <ckdghks9741@gmail.com>
2024-03-11 19:54:59 +0900

    fix: 기숙사에 따른 푸시 알림 컨테이너 대응

--- lib/view/components/w_push_alarm_container.dart
+++ lib/view/components/w_push_alarm_container.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_whattodayrice/models/dormitory.dart';
+import 'package:flutter_whattodayrice/providers/dormitory_provider.dart';
 import 'package:flutter_whattodayrice/theme/colors.dart';
 
-class PushAlarmContainer extends StatelessWidget {
+class PushAlarmContainer extends ConsumerWidget {
   const PushAlarmContainer(
       {super.key,
       required this.isSwitched,
@@ -13,7 +17,8 @@ class PushAlarmContainer extends StatelessWidget {
   final String subtitle;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userDormitory = ref.watch(dormitoryProvider);
     return Container(
       padding: const EdgeInsets.all(20),
       margin: const EdgeInsets.symmetric(vertical: 10.0),
@@ -24,6 +29,17 @@ class PushAlarmContainer extends StatelessWidget {
       ),
       child: Row(
         children: [
+          Visibility(
+            visible: userDormitory != DormitoryType.happiness,
+            child: Padding(
+              padding: EdgeInsets.only(right: 10.w),
+              child: Image.asset(
+                'assets/images/washing-machine.png',
+                width: 30.w,
+                height: 30.h,
+              ),
+            ),
+          ),
           Expanded(
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
