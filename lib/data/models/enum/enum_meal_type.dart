df8d12efcbb5cd38db94e51e21b7881b313dc964
Chang Hwan <ckdghks9741@gmail.com>
2024-09-26 22:53:21 +0900

    add: 식단 디자인 변경에 따른 Data layer 로직 추가 및 수정

---
+++ lib/data/models/enum/enum_meal_type.dart
@@ -0,0 +1,10 @@
+enum MealType {
+  breakfast("조식"),
+  lunch("중식"),
+  dinner("석식"),
+  none("");
+
+  final String title;
+
+  const MealType(this.title);
+}
---
+++ lib/data/models/enum/enum_menu_type.dart
@@ -0,0 +1,9 @@
+enum MenuType {
+  normal("정식"),
+  premium("일품"),
+  takeOut("TAKE-OUT");
+
+  final String title;
+
+  const MenuType(this.title);
+}
