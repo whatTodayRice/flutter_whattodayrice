6ae248bb23083d2924b8e2ba068250dbf26a585f
Chang Hwan <ckdghks9741@gmail.com>
2024-10-26 15:29:03 +0900

    add: 게시글 날짜 converter 추가

--- lib/modules/board/widget/post_list_view.dart
+++ lib/modules/board/widget/post_list_view.dart
@@ -1,6 +1,6 @@
 import 'package:custom_refresh_indicator/custom_refresh_indicator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_whattodayrice/common/utils/app_loading_indicator.dart';
+import 'package:flutter_whattodayrice/common/widget/app_loading_indicator.dart';
 import 'package:flutter_whattodayrice/config/router/route_config.dart';
 import 'package:flutter_whattodayrice/data/models/enum/enum_post_type.dart';
 import 'package:flutter_whattodayrice/data/models/post.dart';
