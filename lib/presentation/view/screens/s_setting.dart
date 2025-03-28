27090b20d6d1a05715d2fd5cc34b70352348389c
Chang Hwan <ckdghks9741@gmail.com>
2025-03-28 20:26:46 +0900

    add: rebase를 위한 임시 수정

--- lib/presentation/view/screens/s_setting.dart
+++ lib/presentation/view/screens/s_setting.dart
@@ -9,7 +9,6 @@ import 'package:flutter_whattodayrice/common/utils/bottom_sheet_utils.dart';
 import 'package:flutter_whattodayrice/presentation/view/components/common/app_elevated_button.dart';
 import 'package:flutter_whattodayrice/presentation/view/components/notification_switch.dart';
 import 'package:adaptive_theme/adaptive_theme.dart';
-import 'package:flutter_whattodayrice/presentation/view/components/w_push_alarm_container.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -111,42 +110,11 @@ class _SettingsScreenState extends State<SettingsScreen> {
               SizedBox(height: 15.h),
               Text('🍚 이런 기능이 추가될거예요.', style: Theme.of(context).textTheme.titleSmall),
               SizedBox(height: 15.h),
-              Expanded(
+              const Expanded(
                 child: SingleChildScrollView(
                   child: Column(
-                    children: [
-                      PushAlarmContainer(
-                        isSwitched: isSwitched,
-                        title: '🍚 이번 달 간식 신청일을 알려드려요.',
-                        subtitle: '지금 눌러서 확인하기.',
+                      // TODO: 나중에 구현
                       ),
-                      PushAlarmContainer(
-                        isSwitched: isSwitched,
-                        title: '🍚 이번 달 간식 수령일을 알려드려요.',
-                        subtitle: '지금 눌러서 확인하기.',
-                      ),
-                      PushAlarmContainer(
-                        isSwitched: isSwitched,
-                        title: '😿 일품이 품절되었어요.',
-                        subtitle: '지금 눌러서 확인하기.',
-                      ),
-                      PushAlarmContainer(
-                        isSwitched: isSwitched,
-                        title: '😿 테이크아웃이 품절되었어요.',
-                        subtitle: '지금 눌러서 확인하기.',
-                      ),
-                      PushAlarmContainer(
-                        isSwitched: isSwitched,
-                        title: '😿 조식 30분 전이에요!',
-                        subtitle: '지금 눌러서 식단 확인하기.',
-                      ),
-                      PushAlarmContainer(
-                        isSwitched: isSwitched,
-                        title: '2번 세탁기 세탁이 완료되었어요!',
-                        subtitle: '세탁물을 찾으러 와주세요.',
-                      ),
-                    ],
-                  ),
                 ),
               ),
             ],
@@ -164,7 +132,6 @@ String buildThemeText(ThemeMode themeMode) {
     case ThemeMode.dark:
       return '다크 모드';
     case ThemeMode.system:
-    default:
       return '시스템 설정';
   }
 }
@@ -178,7 +145,6 @@ ThemeMode convertToThemeMode(AdaptiveThemeMode adaptiveThemeMode) {
       return ThemeMode.dark;
 
     case AdaptiveThemeMode.system:
-    default:
       return ThemeMode.system;
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import app_links
 import cloud_firestore
+import file_selector_macos
 import firebase_core
 import path_provider_foundation
 import shared_preferences_foundation
@@ -17,6 +18,7 @@ import webview_flutter_wkwebview
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
